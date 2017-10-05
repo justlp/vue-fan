a2a54aba2c794bcf91cc0c6badd1aa101ffd5eeb
elpe <elpe@zeoalliance.com>
2017-10-05 15:26:05 +0300

    added extends

--- src/main.js
+++ src/main.js
@@ -3,6 +3,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
+var vendorPrefix = {
+    data: function () {
+        return {
+            browser: 'chrome'
+        }
+    },
+    computed: {
+        getBrowserPrefix: function () {
+            var prefixList = {
+                chrome: '-webkit-',
+                firefox: '-moz-',
+                ie: {
+                    colon: '::',
+                    standard: '-ms-'
+                },
+                opera: '-o-',
+                default: 'def'
+            };
+            if (typeof this.browser !== 'string' && this.browser.name && this.browser.name === 'ie') {
+                if (this.isColon) {
+                    return prefixList.ie.colon
+                } else {
+                    return prefixList.ie.standard
+                }
+            }
+            return typeof prefixList[this.browser] !== 'undefined' ? prefixList[this.browser] : prefixList.default
+        }
+    },
+    template: '<span class="vendor-prefix">{{ getBrowserPrefix }}</span>'
+}
+
 new Vue({
     el: '#app',
     components: {
@@ -48,50 +79,21 @@ new Vue({
             '</span>'
         },
         'pseudo-element': {
+            extends: vendorPrefix,
             data: function () {
-
                 return {
                     name: 'after',
                     isSingleNotation: true,
                     vendorPrefix: 'hz' // todo: get getBrowserPrefix from vendor-prefix component
                 }
             },
-            template: '<span class="pseudo-element" >' +
+            template: '<span class="pseudo-element" >{{getBrowserPrefix}}' +
                 '<span class="pseudo-element__single-notation" v-if="isSingleNotation">:</span>' +
                 '<span class="pseudo-element__single-notation" v-else>::</span>' +
                 '<span class="pseudo-element__name">{{ name }}</span>' +
             '</span>'
         },
-        'vendor-prefix': {
-            data: function () {
-                return {
-                    browser: 'chrome'
-                }
-            },
-            computed: {
-                getBrowserPrefix: function () {
-                    var prefixList = {
-                        chrome: '-webkit-',
-                        firefox: '-moz-',
-                        ie: {
-                            colon: '::',
-                            standard: '-ms-'
-                        },
-                        opera: '-o-',
-                        default: 'def'
-                    };
-                    if (typeof this.browser !== 'string' && this.browser.name && this.browser.name === 'ie') {
-                        if (this.isColon) {
-                            return prefixList.ie.colon
-                        } else {
-                            return prefixList.ie.standard
-                        }
-                    }
-                    return typeof prefixList[this.browser] !== 'undefined' ? prefixList[this.browser] : prefixList.default
-                }
-            },
-            template: '<span class="vendor-prefix">{{ getBrowserPrefix }}</span>'
-        },
+        'vendor-prefix': vendorPrefix,
         'property': {
             data: function () {
                 return {
