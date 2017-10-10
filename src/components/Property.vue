72ad6c32dd828b2926d83b4338216fe002ba6a20
artemlp <artem.just.lp@gmail.com>
2017-10-10 19:41:54 +0300

    added vue component file

---
+++ src/components/Property.vue
@@ -0,0 +1,33 @@
+<template>
+    <span class="property">
+        <span class="property__name">{{ name }}</span>
+        <mark class="mark" v-if="hasSpaceBeforeColon">&nbsp;</mark>
+        <span class="property__colon">:</span>
+        <mark class="mark" v-if="hasSpaceAfterColon">&nbsp;</mark>
+        <span class="property__value">{{ value }}</span>
+        <span class="property__important" v-if="isImportant">!important</span>
+        <span class="property__semicolon" v-if="isSemicolon">;</span>
+    </span>
+</template>
+
+<script>
+    export default {
+      name: 'property',
+      data: function () {
+        return {
+          name: 'color', // or put another component
+          value: '#fff', // or put another component
+          isSemicolon: true,
+          isImportant: true,
+          hasSpaceBeforeColon: true,
+          hasSpaceAfterColon: true
+        }
+      }
+    }
+</script>
+
+<style scoped>
+    span{
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Property from './components/Property'
 
 var vendorPrefix = {
     data: function () {
@@ -33,6 +34,8 @@ var vendorPrefix = {
     },
     template: '<span class="vendor-prefix">{{ getBrowserPrefix }}</span>'
 }
+
+Vue.component(Property.name, Property)
 
 new Vue({
     el: '#app',
@@ -93,27 +96,27 @@ new Vue({
                 '<span class="pseudo-element__name">{{ name }}</span>' +
             '</span>'
         },
-        'vendor-prefix': vendorPrefix,
-        'property': {
-            data: function () {
-                return {
-                    name: 'color', // or put another component
-                    value: '#fff', // or put another component
-                    isSemicolon: true,
-                    isImportant: true,
-                    hasSpaceBeforeColon: true,
-                    hasSpaceAfterColon: true
-                }
-            },
-            template: '<span class="property">' +
-            '<span class="property__name">{{ name }}</span>' +
-            '<mark class="mark" v-if="hasSpaceBeforeColon">&nbsp;</mark>' +
-            '<span class="property__colon">:</span>' +
-            '<mark class="mark" v-if="hasSpaceAfterColon">&nbsp;</mark>' +
-            '<span class="property__value">{{ value }}</span>' +
-            '<span class="property__important" v-if="isImportant">!important</span>' +
-            '<span class="property__semicolon" v-if="isSemicolon">;</span>' +
-            '</span>'
-        }
+        'vendor-prefix': vendorPrefix
+        // 'property': {
+        //     data: function () {
+        //         return {
+        //             name: 'color', // or put another component
+        //             value: '#fff', // or put another component
+        //             isSemicolon: true,
+        //             isImportant: true,
+        //             hasSpaceBeforeColon: true,
+        //             hasSpaceAfterColon: true
+        //         }
+        //     },
+        //     template: '<span class="property">' +
+        //     '<span class="property__name">{{ name }}</span>' +
+        //     '<mark class="mark" v-if="hasSpaceBeforeColon">&nbsp;</mark>' +
+        //     '<span class="property__colon">:</span>' +
+        //     '<mark class="mark" v-if="hasSpaceAfterColon">&nbsp;</mark>' +
+        //     '<span class="property__value">{{ value }}</span>' +
+        //     '<span class="property__important" v-if="isImportant">!important</span>' +
+        //     '<span class="property__semicolon" v-if="isSemicolon">;</span>' +
+        //     '</span>'
+        // }
     }
 });
