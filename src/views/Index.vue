713b464350d20022b0e12631dfad1589b289ac53
elpe <elpe@zeoalliance.com>
2018-06-25 17:17:23 +0300

    ...

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -17,19 +17,29 @@
                     el-radio-button(label="true")
                     el-radio-button(label="false")
 
-                  el-alert(:title="commentNoEmpty === 'true' ? 'good' : 'bad'",
-                           :type="commentNoEmpty === 'true' ? 'success' : 'error'",
-                           :closable="false")
-                    template(v-if="commentNoEmpty === 'true'")
+
+                  template(v-if="commentNoEmpty === 'true'")
+                    el-alert(title="good", type="success", :closable="false")
                       comment(message=" comment ")
                       | or
                       comment(message="\n * Multi-line \Comment \n *")
-                    template(v-else)
+                    el-alert(title="bad", type="error", :closable="false")
                       comment
                       | or
                       comment(message=" ")
                       | or
                       comment(message="\n \n")
+                  template(v-else)
+                    el-alert(title="good", type="success", :closable="false")
+                      comment(message=" comment ")
+                      | or
+                      comment(message="\n * Multi-line \Comment \n *")
+                      comment
+                      | or
+                      comment(message=" ")
+                      | or
+                      comment(message="\n \n")
+
 
           el-col(:span="12")
             el-button(@click="changingCssPropertiesHandle") Reverse
