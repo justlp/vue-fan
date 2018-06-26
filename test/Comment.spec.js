8337967300b9250bc1da1149e3cdff20db62ef8a
artemlp <artem.just.lp@gmail.com>
2018-06-26 23:35:32 +0300

    add unit tests

--- test/Comment.spec.js
+++ test/Comment.spec.js
@@ -1,18 +1,28 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import Comment from '../src/components/Comment.vue'
 
 describe('Comment', () => {
-  it('should be empty comments', () => {
+  it('render /**/ if not passed in slot', () => {
     const wrapper = shallowMount(Comment)
 
     expect(wrapper.text()).toBe('/**/')
   })
 
-  it('should be passed text through slot', () => {
+  it('render slot message when passed in slot', () => {
     const wrapper = shallowMount(Comment, {
       slots: 'simple slot'
     })
 
     expect(wrapper.text()).toBe('/*simple slot*/')
+  })
+
+  it('render component when passed in slot', () => {
+    const wrapper = shallowMount(Comment, {
+      slots: {
+        default: Comment
+      }
+    })
+
+    expect(wrapper.text()).toBe('/*/**/*/')
   })
 })
